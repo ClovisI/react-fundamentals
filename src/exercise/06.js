1f68fbc4bdfc27c8e109c0001cdeb32fe15a153e
ClovisI <pgronstal@hotmail.com>
2023-05-26 04:15:02 -0600

    Finished HW06 Extra Credit 1.  Ref exercise is working 100%

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,19 +2,21 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import { useRef } from 'react';
 
 function UsernameForm({onSubmitUsername}) {
+  let inputRef = useRef(null);
+  
   const handleOnSubmit = (event) =>{
     event.preventDefault();
-    const userName = event.target.elements.userNameInput.value;
-    return alert(`you entered ${userName}`);
+    return alert(`you entered ${inputRef.current.value}`);
   };
 
   return (
     <form onSubmit={handleOnSubmit}>
       <div>
         <label htmlFor="userNameInput">Username:</label>
-        <input id="userNameInput"type="text" />
+        <input ref={inputRef} id="userNameInput"type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
